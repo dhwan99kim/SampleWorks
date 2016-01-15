7d5937a776d4b32fa938a91ca8bf2bc5f6990b12
DonghwanKim <kdh1204@hotmail.com>
2016-01-15 14:08:42 +0900

    Apply parse for push notification

--- app/src/main/java/com/sophism/sampleapp/AppDefine.java
+++ app/src/main/java/com/sophism/sampleapp/AppDefine.java
@@ -12,4 +12,8 @@ public class AppDefine {
     public static final String OAUTH_CLIENT_SECRET = "A1KNlrYiSh";
     public static final String OAUTH_CLIENT_NAME = "샘플 프로젝트";
 
+    //parse key
+    public static final String PARSE_APP_ID = "I8RbwKXqiwLpw6JaWRa37fjt7KuuG8skAAGLe5Hj";		//Parse.com Appplication Key
+    public static final String PARSE_CLIENT_KEY = "8jPBDTHJTFiezFk322d842QBfrdCDgRqSJdjkAEJ";	//Parse.com Client Key
+
 }
---
+++ app/src/main/java/com/sophism/sampleapp/SampleApplication.java
@@ -0,0 +1,32 @@
+package com.sophism.sampleapp;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
+/**
+ * Created by D.H.KIM on 2016. 1. 15.
+ */
+public class SampleApplication extends Application{
+    private final String TAG = "SampleApplication";
+    public static SampleApplication app;
+    public Context mContext;
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Log.v(TAG, "Application OnCreate");
+        mContext = getApplicationContext();
+        app = this;
+        init();
+    }
+
+    public void init(){
+        Parse.initialize(this, AppDefine.PARSE_APP_ID, AppDefine.PARSE_CLIENT_KEY);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+
+
+}
