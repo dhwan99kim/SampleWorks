892423c4bc66cce50d5ac70194346bc39356cf40
DonghwanKim <kdh1204@hotmail.com>
2016-01-18 09:07:54 +0900

    WebBoard - Board Lisst Sample base UI works

---
+++ app/src/main/java/com/sophism/sampleapp/data/BoardArticleData.java
@@ -0,0 +1,39 @@
+package com.sophism.sampleapp.data;
+
+/**
+ * Created by D.H.KIM on 2016. 1. 18.
+ */
+public class BoardArticleData {
+    private String title;
+    private String author;
+    private String article;
+    private String date;
+
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getAuthor(){
+        return title;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public String getArticle(){
+        return article;
+    }
+    public void setArticle(String article){
+        this.article = article;
+    }
+
+    public String getDate(){
+        return date;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+}
--- app/src/main/java/com/sophism/sampleapp/fragments/FragmentBoardSample.java
+++ app/src/main/java/com/sophism/sampleapp/fragments/FragmentBoardSample.java
@@ -1,18 +1,29 @@
 package com.sophism.sampleapp.fragments;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.sophism.sampleapp.R;
+import com.sophism.sampleapp.data.BoardArticleData;
+
+import java.util.ArrayList;
 
 /**
  * Created by D.H.KIM on 2016. 1. 16.
  */
 public class FragmentBoardSample extends Fragment {
 
+    ArrayList<BoardArticleData> mBoardArticles = new ArrayList<>();
+    BoardListAdapter mAdapter;
+    ListView mBoardListView;
+    Context mContext;
     public FragmentBoardSample(){
 
     }
@@ -21,7 +32,67 @@ public class FragmentBoardSample extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle instance) {
 
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_board_sample, container, false);
+        mContext = getActivity();
+        mBoardListView = (ListView)rootView.findViewById(R.id.listview_board);
+        mAdapter = new BoardListAdapter(mContext);
+        mBoardListView.setAdapter(mAdapter);
 
+        updateBoardArticle();
         return rootView;
+    }
+
+    private void updateBoardArticle(){
+        if (mBoardArticles.size() == 0){
+            mBoardListView.setVisibility(View.GONE);
+        }
+    }
+    private class BoardListAdapter extends BaseAdapter{
+        private LayoutInflater mInflater;
+
+        public BoardListAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return mBoardArticles.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mBoardArticles.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = mInflater.inflate(R.layout.list_item_board_article, parent, false);
+                holder.board_article_title = (TextView) convertView.findViewById(R.id.board_article_title);
+                holder.board_article_author = (TextView) convertView.findViewById(R.id.board_article_author);
+                holder.board_article_date = (TextView) convertView.findViewById(R.id.board_article_date);
+                holder.board_article_article = (TextView) convertView.findViewById(R.id.board_article_article);
+            }else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            holder.board_article_title.setText(mBoardArticles.get(position).getTitle());
+            holder.board_article_author.setText(mBoardArticles.get(position).getAuthor());
+            holder.board_article_date.setText(mBoardArticles.get(position).getDate());
+            holder.board_article_article.setText(mBoardArticles.get(position).getArticle());
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView board_article_title;
+            TextView board_article_author;
+            TextView board_article_article;
+            TextView board_article_date;
+        }
     }
 }
